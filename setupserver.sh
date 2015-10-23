296fcad62fd4020c27dcdf83a94aff5417dbeeb6
Stephen Wade <stephen@stephenwade.me>
2015-10-23 12:23:13 -0400

    Changed nodejs path to /game

--- setupserver.sh
+++ setupserver.sh
@@ -21,6 +21,6 @@ wget -qO- https://npmjs.com/install.sh | sudo bash -
 sudo npm install pm2 -g
 sudo pm2 startup ubuntu
 
-sudo sed -i -e '/\s*#.*$/d' -e '/^\s*$/d' -e 's/server_name _/server_name acquire.stephenwade.me/' -e '/^}$/d' /etc/nginx/sites-available/default
-sudo bash -c 'printf "\tlocation /acquire {\n\t\tproxy_set_header Upgrade \$http_upgrade;\n\t\tproxy_set_header Connection "upgrade";\n\t\tproxy_http_version 1.1;\n\t\tproxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;\n\t\tproxy_set_header Host \$host;\n\t\tproxy_pass http://127.0.0.1:8001;\n\t}\n}" >> /etc/nginx/sites-available/default'
+sudo sed -i -e '/\s*#.*$/d' -e '/^\s*$/d' -e 's:root /var/www/html:root /var/www/acquiregame/client:' -e 's/server_name _/server_name acquire.stephenwade.me/' -e '/^}$/d' /etc/nginx/sites-available/default
+sudo bash -c 'printf "\tlocation /game {\n\t\tproxy_set_header Upgrade \$http_upgrade;\n\t\tproxy_set_header Connection "upgrade";\n\t\tproxy_http_version 1.1;\n\t\tproxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;\n\t\tproxy_set_header Host \$host;\n\t\tproxy_pass http://127.0.0.1:8001;\n\t}\n}" >> /etc/nginx/sites-available/default'
 sudo systemctl restart nginx
