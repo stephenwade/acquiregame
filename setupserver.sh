81688b2b2eb0793dd67c390802d0cbe134d825d9
Stephen Wade <stephen@stephenwade.me>
2015-10-23 03:09:52 -0400

    Adding setup script to set up new DigitalOcean server

---
+++ setupserver.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This is a script that will set up node.js and nginx on a DigitalOcean server running Ubuntu 15.10.
+# It also installs pm2, which is a process manager for node.js.
+# Use it like `pm2 start acquire.js`
+
+sudo touch /var/swap.img
+sudo chmod 600 /var/swap.img
+sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=1000
+sudo mkswap /var/swap.img
+sudo bash -c 'echo "/var/swap.img none swap sw 0 0" >> /etc/fstab'
+sudo swapon /var/swap.img
+
+sudo apt update
+sudo apt -y full-upgrade
+
+sudo apt -y install vim curl git nginx nodejs npm nodejs-legacy
+
+sudo npm install npm -g
+sudo npm install pm2 -g
+sudo pm2 startup ubuntu
+
+sudo sed -i -e '/\s*#.*$/d' -e '/^\s*$/d' -e 's/server_name _/server_name acquire.stephenwade.me/' -e '$d' /etc/nginx/sites-available/default
+sudo bash -c 'printf "\tlocation /acquire {\n\t\tproxy_set_header Upgrade $http_upgrade;\n\t\tproxy_set_header Connection "upgrade";\n\t\tproxy_http_version 1.1;\n\t\tproxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n\t\tproxy_set_header Host $host;\n\t\tproxy_pass http://127.0.0.1:8001;\n\t}\n}" >> /etc/nginx/sites-available/default'
+sudo systemctl restart nginx
