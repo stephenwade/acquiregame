46d66c549835c1f4d9436c2c360c4bd4fd65ea65
Stephen Wade <stephen@stephenwade.me>
2015-10-23 22:32:58 -0400

    Simple changes to test GitHub webhook

--- server/acquire.js
+++ server/acquire.js
@@ -7,12 +7,10 @@ var mime = require("mime");
 http.createServer(function (request, response) {
   let url = request.url;
   console.log(url);
-  if (url == "/")
-    url = "/index.html"
   
   if (url.startsWith("/game")) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("server-side logic goes here");
+    response.end("coming soon");
   } else {
     response.writeHead(404, {"Content-Type": "text/plain"});
     response.end("how did you get here??");
