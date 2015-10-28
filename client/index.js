061701109b83867c4e523fc498082caa4937dc6c
Stephen Wade <stephen@stephenwade.me>
2015-10-27 21:34:28 -0400

    Beginnings of an actual router

--- client/index.js
+++ client/index.js
@@ -1,21 +1,21 @@
-'use strict';
+// 'use strict';
 
-window.addEventListener("WebComponentsReady", function(e) { // page ready to go
-  var socket = io('10.9.43.188:8001');
+// window.addEventListener("WebComponentsReady", function(e) { // page ready to go
+//   var socket = io('10.9.43.188:8001');
   
-  $('paper-button').click(function(evt) {
-    var input = $('paper-input')[0];
-    var val = input.value;
+//   $('paper-button').click(function(evt) {
+//     var input = $('paper-input')[0];
+//     var val = input.value;
     
-    if (val !== '') {
-      socket.emit('chat message', val);
-      console.log('submitting:', val);
-      input.value = '';
-    }
-  });
+//     if (val !== '') {
+//       socket.emit('chat message', val);
+//       console.log('submitting:', val);
+//       input.value = '';
+//     }
+//   });
   
-  socket.on('chat message', function(msg) {
-    console.log(msg);
-    $('#messages').append($('<p>').text(msg));
-  });
-});
+//   socket.on('chat message', function(msg) {
+//     console.log(msg);
+//     $('#messages').append($('<p>').text(msg));
+//   });
+// });
