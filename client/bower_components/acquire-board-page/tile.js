d789aed7fc6594376b588b1cc16d7b1fa50d260f
William Ashton <William@AshtonFam.org>
2015-11-11 21:57:05 -0500

    Move cell display logic into a different class

---
+++ client/bower_components/acquire-board-page/tile.js
@@ -0,0 +1,68 @@
+'use strict';
+
+class BoardCell {
+  constructor(context, row, col, size) {
+    this.context = context;
+    this.row = row;
+    this.col = col;
+    this.calcDimensions(size);
+    
+    this.filled = false;
+    
+    this.colors = {
+      borderColor:      '#555555',
+      emptyBackground:  '#ffffff',
+      filledBackground: '#555555',
+      emptyText:        '#000000',
+      filledText:       '#ffffff'
+    };
+  }
+  
+  calcDimensions(size) {
+    this.x = size * this.col;
+    this.y = size * this.row;
+    this.size = size;
+    this.width = size * 0.9;
+    this.padding = size * 0.05;
+  }
+  
+  draw(size) {
+    this.calcDimensions(size);
+    
+    this.drawOuterBox();
+    this.drawInnerBox();
+    this.drawText();
+  }
+  
+  drawOuterBox() {
+    this.context.strokeStyle = this.colors.borderColor;
+    
+    this.context.strokeRect(this.x, this.y, this.size, this.size);
+  }
+  
+  drawInnerBox() {
+    let id = this.filled ? 'filledBackground' : 'emptyBackground';
+    
+    this.context.fillStyle = this.colors[id];
+    this.context.fillRect(this.x + this.padding,
+                          this.y + this.padding,
+                          this.width,
+                          this.width);
+  }
+  
+  drawText() {  
+    let id = this.filled ? 'filledText' : 'emptyText';
+    
+    this.context.fillStyle = this.colors[id];
+    this.context.font = (this.size / 3).toString() + 'px sans';
+    this.context.textAlign = 'center';
+    
+    this.context.fillText(this.cellText(),
+                          this.x + this.size / 2,
+                          this.y + this.size * (3 / 5))
+  }
+  
+  cellText() {
+    return (this.col + 1) + '-' + String.fromCharCode(65 + this.row);
+  }
+}
--- client/bower_components/acquire-board-page/view.js
+++ client/bower_components/acquire-board-page/view.js
@@ -7,11 +7,15 @@ class BoardView {
     
     this.frame = 0;
     this.board = [];
+    
+    var size = Math.min(this.canvas.width / 12, this.canvas.height / 9);
+    
     for (let x = 0; x < 12; x++) {
       this.board.push([]);
       
       for (let y = 0; y < 9; y++) {
-        this.board[x][y] = Math.random() >= 0.5;
+        this.board[x][y] = new BoardCell(this.context, y, x, size);
+        this.board[x][y].filled = Math.random() >= 0.5;
       }
     }
   }
@@ -39,38 +43,10 @@ class BoardView {
     
     for (let x = 0; x < 12; x++) {
       for (let y = 0; y < 9; y++) {
-        this.drawSpace({x, y}, size, this.board[x][y]);
+        //this.drawSpace({x, y}, size, this.board[x][y]);
+        this.board[x][y].draw(size);
       }
     }
-  }
-  
-  drawSpace(cell, size, filled) {
-    let width = size * 0.90;
-    let padding = size * 0.05;
-    let text = this.cellText(cell);
-    let textColor;
-    
-    if (filled) {
-      textColor = '#ffffff';
-      
-      this.context.fillStyle = '#555555';
-      this.context.fillRect(size * cell.x + padding, size * cell.y + padding, width, width);
-    } else {
-      textColor = '#000000';
-      
-      this.context.strokeRect(size * cell.x + padding, size * cell.y + padding, width, width);
-    }
-    this.context.strokeStyle = '#555555';
-    this.context.strokeRect(size * cell.x, size * cell.y, size, size);
-    
-    this.context.fillStyle = textColor;
-    this.context.font = (size / 3).toString() + 'px sans';
-    this.context.textAlign = 'center';
-    this.context.fillText(text, size * cell.x + size / 2, size * cell.y + size * (3 / 5));
-  }
-  
-  cellText(cell) {
-    return (cell.x + 1) + '-' + String.fromCharCode(65 + cell.y);
   }
   
   resize() {
