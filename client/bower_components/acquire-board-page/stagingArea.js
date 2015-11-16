650589c069fd0b9fdf2fed8cfdd67dbe91148913
William Ashton <William@AshtonFam.org>
2015-11-16 00:08:44 -0500

    Change text color when tile flips.

--- client/bower_components/acquire-board-page/stagingArea.js
+++ client/bower_components/acquire-board-page/stagingArea.js
@@ -1,33 +1,24 @@
 'use strict';
 
 class StagingArea {
-  constructor(text, size) {
+  constructor(text, color) {
     this.canvas = document.createElement('canvas');
     this.context = this.canvas.getContext('2d');
     
     this.text = text;
-    
-    this.colors = {
-      borderColor:      '#555555',
-      emptyBackground:  '#ffffff',
-      filledBackground: '#555555',
-      emptyText:        '#000000',
-      filledText:       '#ffffff'
-    };
+    this.color = color;
   }
   
   draw(size) {
     this.canvas.width = size;
     this.canvas.height = size;
     
-    console.log('drawing');
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.context.clearRect(0, 0, size, size);
     
-    this.context.fillStyle = '#000000';
+    this.context.fillStyle = this.color;
     this.context.font = (size / 3).toString() + 'px Roboto';
     this.context.textAlign = 'center';
     
-    this.context.fillText(this.text,
-                          size / 2, size * (3/5));
+    this.context.fillText(this.text, size / 2, size * (3/5));
   }
 }
